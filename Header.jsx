75909d760b252870dd53b92d8c2bd5aac64c64d0
Vishwa-B <86521976+Vishwa-B@users.noreply.github.com>
2021-06-27 14:07:28 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Basic Front End Web Dev with Javascript and React.js</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>This Bootcamp gave me a good knowledge for working a Web development.Me
+        covered everything from Scratch including Javascript React.js,HTML.</p>
+    </div>
+  );
+}
+
+export default Info;
